bb95185bd5e2bc4e27576e19c65bb4f19bf53834
redenv <redenv@careindeed.com>
2023-04-15 19:07:10 +0800

    Stack - Exercise01

---
+++ Stacks/Exercise01.java
@@ -0,0 +1,45 @@
+// Exercise 1: Check if a string of parentheses is balanced, meaning each opening parenthesis has a corresponding closing parenthesis in the correct order. The output of each check should be either true or false. Use the following examples to be evaluated:
+//  parentheses1 = ( ( ( ) ) )
+//  parentheses2 = ( ( ) ( ) )
+//  parentheses3 = ( ( )
+//  parentheses4 = ( ) )
+
+import java.util.Stack;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        String p1 = "((()))";
+        String p2 = "(()())";
+        String p3 = "(()";
+        String p4 = "())";
+
+        System.out.println("p1: " + isBalanced(p1)); // Output: true
+        System.out.println("p2: " + isBalanced(p2)); // Output: true
+        System.out.println("p3: " + isBalanced(p3)); // Output: false
+        System.out.println("p4: " + isBalanced(p4)); // Output: false
+    }
+
+    // Checks if a string of parentheses is balanced.
+    public static boolean isBalanced(String s) {
+        // Create a stack to store opening parentheses.
+        Stack<Character> stack = new Stack<>();
+
+        // Iterate through the characters in the input string.
+        for (char c : s.toCharArray()) {
+            // If the character is an opening parenthesis, push it onto the stack.
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                // If the character is a closing parenthesis, check if the stack is empty
+                // or if the top element is not an opening parenthesis.
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    // In either case, return false (unbalanced).
+                    return false;
+                }
+            }
+        }
+
+        // If the stack is empty, the string is balanced; otherwise, it is unbalanced.
+        return stack.isEmpty();
+    }
+}
